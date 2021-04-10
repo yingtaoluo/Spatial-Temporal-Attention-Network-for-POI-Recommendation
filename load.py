1fcbd436dc45624d7af11248b44f4972adc81278
Ying-Tao Luo <yl3851@uw.edu>
2021-04-10 09:25:33 +0800

    Update load.py

--- load.py
+++ load.py
@@ -17,7 +17,6 @@ def haversine(lon1, lat1, lon2, lat2):
     """
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    # haversine公式
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
